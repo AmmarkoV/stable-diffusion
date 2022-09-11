c8bfe19380bf8deb0bae04c8487df8aa6e5d3eab
Ammar Qammaz <ammarkov@gmail.com>
2022-09-11 03:15:41 +0300

    ...

--- initialize.sh
+++ initialize.sh
@@ -13,10 +13,11 @@ source stablevenv/bin/activate
 python3 -m pip install -r requirements.txt --extra-index-url https://download.pytorch.org/whl/cu116
 
 
-python3 -m pip install transformers==4.19.2 diffusers invisible-watermark
+#python3 -m pip install transformers==4.19.2 diffusers invisible-watermark
 python3 -m pip install requirements.txt
 python3 -m pip install -e git+https://github.com/CompVis/taming-transformers.git@master#egg=taming-transformers
 python3 -m pip install -e git+https://github.com/openai/CLIP.git@main#egg=clip
 python3 -m pip install -e .
+pip install --upgrade packaging
 
 exit 0
