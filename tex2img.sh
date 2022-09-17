9be7c5cd520a2447f1fddccd209c667e23d67dfb
Ammar Qammaz <ammarkov@gmail.com>
2022-09-17 20:05:17 +0300

    ...

--- tex2img.sh
+++ tex2img.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+
+
 SEED=`date +%s`
-python scripts/txt2img.py --prompt "$@" --n_samples 15 --seed $SEED --plms && scp -P 2222 outputs/txt2img-samples/samples/* ammar@ammar.gr:/home/ammar/public_html/stable-diffusion/outputs/txt2img-samples/samples
+python scripts/txt2img.py --prompt "$@" --n_samples 4 --seed $SEED --plms #&& scp -P 2222 outputs/txt2img-samples/samples/* ammar@ammar.gr:/home/ammar/public_html/stable-diffusion/outputs/txt2img-samples/samples
 
 
 exit 0
