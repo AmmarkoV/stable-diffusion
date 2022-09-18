c09932fadce0e0082f35b33514666247afcb00da
Ammar Qammaz <ammarkov@gmail.com>
2022-09-18 18:55:08 +0300

    ...

--- tex2img.sh
+++ tex2img.sh
@@ -7,7 +7,7 @@ cd "$DIR"
 rm outputs/txt2img-samples/samples/*.png
 
 SEED=`date +%s`
-python scripts/txt2img.py --prompt "$@" --n_samples 2 --seed $SEED --plms
+python scripts/txt2img.py --prompt "$@" --n_samples 2 --seed $SEED --plms --n_iter 6
 echo "QUERY($@) SEED($SEED)" >> log.txt
 
 exit 0
