eb94dcbaf1e4ad18d363b137f0e0367cbc38a031
Ammar Qammaz <ammarkov@gmail.com>
2022-09-18 20:41:53 +0300

    ...

--- demo.sh
+++ demo.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 source stablevenv/bin/activate
-AmmarServer/scripts/run_imagegeneration
+
+until false; do
+    AmmarServer/scripts/run_imagegeneration
+    echo "Server 'Image Generation' stopped with exit code $?.  Respawning.." >&2
+    sleep 0.1
+done
+  
+
 
 exit 0
