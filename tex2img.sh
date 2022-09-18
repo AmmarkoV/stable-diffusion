b899d722ac5c6721544d7eb7d1d1c1618ed8ab6b
Ammar Qammaz <ammarkov@gmail.com>
2022-09-18 14:38:17 +0300

    ...

--- tex2img.sh
+++ tex2img.sh
@@ -5,6 +5,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 
 
+ps_out=`ps -ef | grep $1 | grep -v 'grep' | grep -v tex2img.sh`
+result=$(echo $ps_out | grep "$1")
+if [[ "$result" != "" ]];then
+    echo "Running"
+    exit 0
+else
+    echo "Not Running"
+fi 
+
 SEED=`date +%s`
 python scripts/txt2img.py --prompt "$@" --n_samples 4 --seed $SEED --plms
 echo "QUERY($@) SEED($SEED)" >> log.txt
