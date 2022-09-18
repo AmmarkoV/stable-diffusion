271439b2c82bcc19c49577d88b737e45b532a345
Ammar Qammaz <ammarkov@gmail.com>
2022-09-18 15:21:49 +0300

    ...

--- tex2imgOnlyOnce.sh
+++ tex2imgOnlyOnce.sh
@@ -5,7 +5,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 
 
-result=`ps -ef | grep tex2img.sh` | grep -v "grep"
+#result=`ps -ef | grep tex2img.sh` | grep -v "grep"
+ps_out=`ps -ef | grep "tex2img.sh" | grep -v 'grep' | grep -v $0`
+result=$(echo $ps_out | grep "$1")
 if [ -z "$result" ]; 
 then
     echo "Not Running"
