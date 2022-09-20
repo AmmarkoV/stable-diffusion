f2ed53012e9427c0c86d7f235a601f11b4270f13
Ammar Qammaz <ammarkov@ics.forth.gr>
2022-09-20 13:27:49 +0300

    ...

--- tex2img.sh
+++ tex2img.sh
@@ -15,7 +15,7 @@ then
 else
  rm outputs/txt2img-samples/samples/*.png
  rm outputs/txt2img-samples-laion400m/samples/*.png
- python scripts/txt2img.py --prompt "$@" --n_samples 2 --seed $SEED --plms --ddim_steps 100 --n_iter 1 --laion400m 
+ python scripts/txt2img.py --prompt "$@" --n_samples 2 --seed $SEED --plms --ddim_steps 100 --laion400m 
  mv outputs/txt2img-samples-laion400m/samples/*.png outputs/txt2img-samples/samples/
 fi  
 
