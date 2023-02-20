56d12cd3e537a87665036aa0ecb057837a397867
Ammar Qammaz <ammarkov@ics.forth.gr>
2023-02-20 16:30:50 +0200

    img2img.sh now has programmable strength..

--- img2img.sh
+++ img2img.sh
@@ -5,20 +5,20 @@ cd "$DIR"
  
 SEED=`date +%s`
 
-isHQ=`echo "$@" | grep 'laion400m'`
-echo "QUERY($@) SEED($SEED) HQ($isHQ)" >> log.txt
+isHQ=`echo "$2" | grep 'laion400m'`
+echo "QUERY($2) SEED($SEED) HQ($isHQ)" >> log.txt
 
 if [ -z "$isHQ" ]; 
 then
  rm outputs/txt2img-samples/samples/*.png
  rm outputs/img2img-samples/samples/*.png
- python scripts/img2img.py --prompt "$@" --init-img upimage.jpg --strength 0.4 --n_samples 4 --seed $SEED --ddim_steps 100
+ python scripts/img2img.py --prompt "$2" --init-img upimage.jpg --strength $1 --n_samples 4 --seed $SEED --ddim_steps 100
  mv outputs/img2img-samples/samples/*.png outputs/txt2img-samples/samples/
 else
  rm outputs/txt2img-samples/samples/*.png
  rm outputs/img2img-samples/samples/*.png
  rm outputs/img2img-samples-laion400m/samples/*.png
- python scripts/img2img.py --prompt "$@" --init-img upimage.jpg --strength 0.4 --n_samples 4 --seed $SEED --ddim_steps 100 --laion400m 
+ python scripts/img2img.py --prompt "$2" --init-img upimage.jpg --strength $1 --n_samples 4 --seed $SEED --ddim_steps 100 --laion400m 
  mv outputs/img2img-samples-laion400m/samples/*.png outputs/txt2img-samples/samples/
 fi  
 
